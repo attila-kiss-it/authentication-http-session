160a14de5c97eb9e510b8ddcd3217ae6241eb7da
zsigmond-czine <zsigmond.czine@everit.biz>
2015-04-21 08:14:13 +0200

    Fixed parent version. Fixed serializable problem. Update license.

--- core/src/main/java/org/everit/osgi/authentication/http/session/internal/SessionAuthenticationComponent.java
+++ core/src/main/java/org/everit/osgi/authentication/http/session/internal/SessionAuthenticationComponent.java
@@ -1,32 +1,33 @@
-/**
- * This file is part of Everit - HTTP Session based authentication.
+/*
+ * Copyright (C) 2011 Everit Kft. (http://www.everit.biz)
  *
- * Everit - HTTP Session based authentication is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Everit - HTTP Session based authentication is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Everit - HTTP Session based authentication.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.everit.osgi.authentication.http.session.internal;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -82,10 +83,7 @@ import org.osgi.service.log.LogService;
 })
 @Service
 public class SessionAuthenticationComponent
-    extends HttpServlet
-    implements Filter, AuthenticationSessionAttributeNames {
-
-  private static final long serialVersionUID = 5302724920732803866L;
+    implements Filter, AuthenticationSessionAttributeNames, Servlet {
 
   @Reference(bind = "setAuthenticationPropagator")
   private AuthenticationPropagator authenticationPropagator;
@@ -98,6 +96,8 @@ public class SessionAuthenticationComponent
   private String reqParamNameLoggedOutUrl;
 
   private String sessionAttrNameAuthenticatedResourceId;
+
+  private ServletConfig config;
 
   @Activate
   public void activate(final BundleContext context, final Map<String, Object> componentProperties)
@@ -185,16 +185,13 @@ public class SessionAuthenticationComponent
   }
 
   @Override
-  protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-      throws ServletException,
-      IOException {
-    logout(req, resp);
+  public ServletConfig getServletConfig() {
+    return config;
   }
 
   @Override
-  protected void doPost(final HttpServletRequest req, final HttpServletResponse resp)
-      throws ServletException, IOException {
-    logout(req, resp);
+  public String getServletInfo() {
+    return "";
   }
 
   private String getStringProperty(final Map<String, Object> componentProperties,
@@ -211,6 +208,11 @@ public class SessionAuthenticationComponent
   public void init(final FilterConfig filterConfig) throws ServletException {
   }
 
+  @Override
+  public void init(final ServletConfig pConfig) throws ServletException {
+    config = pConfig;
+  }
+
   private void logout(final HttpServletRequest req, final HttpServletResponse resp)
       throws IOException {
 
@@ -225,10 +227,26 @@ public class SessionAuthenticationComponent
 
     String reqLoggedOutUrl = req.getParameter(reqParamNameLoggedOutUrl);
     if (reqLoggedOutUrl != null) {
-      resp.sendRedirect(reqLoggedOutUrl);
+      resp.sendRedirect(URLEncoder.encode(reqLoggedOutUrl, "UTF-8"));
     } else {
       resp.sendRedirect(loggedOutUrl);
     }
+  }
+
+  @Override
+  public void service(final ServletRequest req, final ServletResponse res) throws ServletException,
+      IOException {
+    HttpServletRequest request;
+    HttpServletResponse response;
+
+    if (!((req instanceof HttpServletRequest) && (res instanceof HttpServletResponse))) {
+      throw new ServletException("non-HTTP request or response");
+    }
+
+    request = (HttpServletRequest) req;
+    response = (HttpServletResponse) res;
+
+    logout(request, response);
   }
 
   public void setAuthenticationPropagator(final AuthenticationPropagator authenticationPropagator) {
--- tests/src/main/java/org/everit/osgi/authentication/http/session/tests/SessionAuthenticationTestComponent.java
+++ tests/src/main/java/org/everit/osgi/authentication/http/session/tests/SessionAuthenticationTestComponent.java
@@ -1,18 +1,17 @@
-/**
- * This file is part of Everit - HTTP Session based authentication tests.
+/*
+ * Copyright (C) 2011 Everit Kft. (http://www.everit.org)
  *
- * Everit - HTTP Session based authentication tests is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Everit - HTTP Session based authentication tests is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Everit - HTTP Session based authentication tests.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.everit.osgi.authentication.http.session.tests;
 
@@ -143,8 +142,8 @@ public class SessionAuthenticationTestComponent {
     StringWriter writer = new StringWriter();
     IOUtils.copy(inputStream, writer);
     String[] responseBodyAsString = writer.toString().split(":");
-    long actualResourceId = Long.valueOf(responseBodyAsString[0]).longValue();
-    long newResourceId = Long.valueOf(responseBodyAsString[1]).longValue();
+    long actualResourceId = Long.parseLong(responseBodyAsString[0]);
+    long newResourceId = Long.parseLong(responseBodyAsString[1]);
     String st = responseBodyAsString.length == 3 ? responseBodyAsString[2] : "should be success";
     Assert.assertEquals(st.replaceAll("-->", ":"), expectedResourceId, actualResourceId);
     return newResourceId;
@@ -206,7 +205,7 @@ public class SessionAuthenticationTestComponent {
 
     sessionResourceId = hello(httpContext, authenticationContext.getDefaultResourceId());
     sessionResourceId = hello(httpContext, sessionResourceId);
-    sessionResourceId = hello(httpContext, sessionResourceId);
+    hello(httpContext, sessionResourceId);
     logoutGet(httpContext);
 
     hello(httpContext, authenticationContext.getDefaultResourceId());
